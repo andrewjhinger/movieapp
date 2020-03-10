14291ca11c857c9f89465c2a18424e61bc3555a6
Andrew Hinger <andrewjhinger@gmail.com>
2020-03-10 01:52:47 +0100

    debound and render movies

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hi there!');
+const fetchData = async searchText => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: { apikey: "12f0cf1f", s: searchText }
+  });
+  if (response.data.Error) return [];
+  return response.data.Search;
+};
+
+const searchInput = document.querySelector("input");
+
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+  console.log(movies);
+
+  const movieSection = document.querySelector("#movies");
+  for (let movie of movies) {
+    const movieDiv = document.createElement("div");
+    movieDiv.innerHTML = `
+    <img src="${movie.Poster}" />
+    <h1>${movie.Title}</h1>
+    `;
+    movieSection.append(movieDiv);
+  }
+};
+searchInput.addEventListener("input", debounce(onInput, 500));
