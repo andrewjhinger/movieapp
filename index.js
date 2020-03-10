75c480ffbf526bc81407a07510b17ead83957a50
Andrew Hinger <andewjhinger@gmail.com>
2020-03-10 11:39:54 -0400

    decouple summaries

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const onMovieSelect = async movie => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: { apikey: "12f0cf1f", i: movie.imdbID }
-  });
-  if (response.data.Error) return {};
-  const summary = document.querySelector("#summary");
-  summary.innerHTML = movieTemplate(response.data);
-};
-
-createAutoComplete({
-  root: document.querySelector(".autocomplete"),
+const autoCompleteConfig = {
   renderOption: movie => {
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
 
     return `
-    <img src="${imgSrc}" />
-    ${movie.Title} (${movie.Year})
-    `;
-  },
-  onOptionSelect: movie => {
-    onMovieSelect(movie);
+  <img src="${imgSrc}" />
+  ${movie.Title} (${movie.Year})
+  `;
   },
   inputValue: movie => {
     return movie.Title;
@@ -30,7 +17,33 @@ createAutoComplete({
     if (response.data.Error) return [];
     return response.data.Search;
   }
+};
+
+createAutoComplete({
+  root: document.querySelector("#left-autocomplete"),
+  ...autoCompleteConfig,
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#left-summary"));
+  }
 });
+
+createAutoComplete({
+  root: document.querySelector("#right-autocomplete"),
+  ...autoCompleteConfig,
+  onOptionSelect: movie => {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#right-summary"));
+  }
+});
+
+const onMovieSelect = async (movie, summaryElement) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: { apikey: "12f0cf1f", i: movie.imdbID }
+  });
+  if (response.data.Error) return {};
+  summaryElement.innerHTML = movieTemplate(response.data);
+};
 
 const movieTemplate = movieDetail => {
   return `
