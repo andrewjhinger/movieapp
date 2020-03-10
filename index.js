9aa5bb81548bb21cb8055baf19b9890c2a7a19f7
Andrew Hinger <andewjhinger@gmail.com>
2020-03-10 08:40:27 -0400

    move html generation and handle broken images

--- index.js
+++ index.js
@@ -6,20 +6,35 @@ const fetchData = async searchText => {
   return response.data.Search;
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search for a Movie</b></label>
+<input class="input" />
+<div class="dropdown">
+  <div class="dropdown-menu">
+    <div class="dropdown-content results"></div>
+  </div>
+</div>
+`;
 const searchInput = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
   console.log(movies);
 
-  const movieSection = document.querySelector("#movies");
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
-    const movieDiv = document.createElement("div");
-    movieDiv.innerHTML = `
-    <img src="${movie.Poster}" />
-    <h1>${movie.Title}</h1>
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+    <img src="${imgSrc}" />
+    ${movie.Title}
     `;
-    movieSection.append(movieDiv);
+    resultsWrapper.append(option);
   }
 };
 searchInput.addEventListener("input", debounce(onInput, 500));
